52ef7bf94d5d2bfa4859bd28f98086a436ed1db0
Brandon <blstrzyzewski@gmail.com>
2020-07-07 15:31:33 -0400

    initialized repo

---
+++ src/__init__.py
@@ -0,0 +1,221 @@
+from flask import Flask, render_template, url_for,flash,make_response,request,redirect,jsonify,Response,send_file,send_from_directory,after_this_request,session
+import os
+import requests
+from werkzeug.utils import secure_filename
+from pysndfx import AudioEffectsChain
+import soundfile as sf
+import numpy as np
+import io
+
+
+from pydub import AudioSegment
+app = Flask(__name__)
+ALLOWED_EXTENSIONS = set(['mp3','wav','m4a','flac'])
+
+UPLOAD_FOLDER = './uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+
+
+@app.route('/')
+def index():
+    '''
+
+    render homepage
+
+    '''
+    return render_template('index.html')
+
+
+def allowed_file(filename):
+    '''
+
+    verify file is compatible type
+
+    '''
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def add_effects(song,type,intensity):
+    '''
+
+    adds effects to pydub audio segment given type and intensity as
+    integer values from request body
+
+    '''
+    file_buffer=io.BytesIO()
+    song_array=as_to_array(song)
+
+
+    #Create effects chans using pysndfx
+    slowed_reverb = (
+        AudioEffectsChain()
+
+        .reverb(wet_gain=2*intensity)
+        .speed(1-(0.06*intensity))
+
+    )
+    bass_boost=(
+
+      AudioEffectsChain()
+      .equalizer(80,0.6,2.5*intensity)
+    )
+    distortion=(
+      AudioEffectsChain()
+      .overdrive(gain=7*intensity)
+
+    )
+    speed_increase=(
+      AudioEffectsChain()
+      .speed(1+0.33*intensity)
+
+    )
+
+    effects=[slowed_reverb,bass_boost,distortion,speed_increase]
+
+    #add effects to each channel individually
+    left_channel=effects[type](song_array[:,0])
+    right_channel=effects[type](song_array[:,1])
+
+
+
+    song_with_effects=array_to_as(left_channel,right_channel,song.frame_rate,song.sample_width)
+
+
+    song_with_effects.export(file_buffer,format="mp3",bitrate="320k")
+
+    file_buffer.seek(0)
+    flash('File successfully uploaded')
+    return file_buffer
+
+
+@app.route('/download',methods=['POST'])
+def download():
+    '''
+
+    downloads mp3 from youtube based api into file buffer.
+    adds effects to this downloaded song
+    returns mp3 with effects and name of file
+
+    '''
+
+
+    url = "https://youtube-to-mp32.p.rapidapi.com/yt_to_mp3"
+
+    querystring = {"video_id":str(request.form["id"])}
+
+    headers = {
+        'x-rapidapi-host': "youtube-to-mp32.p.rapidapi.com",
+        
+        }
+    # get download url from api
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    # download mp3 into file buffer
+    file= requests.request('GET',response.json().get("Download_url"),allow_redirects=True)
+    file_buffer=io.BytesIO(file.content)
+    file_buffer.seek(0)
+
+    #audio segment from downloaded mp3
+    song = AudioSegment.from_mp3(file_buffer)
+
+
+
+    processed_song=add_effects(song,int(request.form["type"]),int(request.form["intensity"]))
+    new_response=make_response(send_file(processed_song,mimetype="audio/mp3"))
+    new_response.headers["name"]=response.json().get("Title")
+    return new_response
+
+
+def as_to_array(audio_segment):
+    '''
+
+    converts pydub audiosegment to an array of samples of shape
+    (length, channels)
+
+
+    '''
+    audio_segment.set_channels(2)
+    samples = audio_segment.get_array_of_samples()
+    samples = np.array(samples)
+    samples = samples.reshape(audio_segment.channels, -1, order='F');
+    samples=samples.transpose() # (<probably 2>, <len(song)
+    return samples
+
+
+def array_to_as(left_array,right_array,frame_rate,sample_width):
+    '''
+
+    converts arrays for left and right audio channels to a stereo pydub
+    audiosegment.
+
+    '''
+    left = AudioSegment(
+        left_array.tobytes(),
+
+        frame_rate=frame_rate,
+        sample_width=sample_width,
+        channels=1
+     )
+    right=AudioSegment(
+         right_array.tobytes(),
+
+         frame_rate=frame_rate,
+         sample_width=sample_width,
+         channels=1
+      )
+    audio_segment=AudioSegment.from_mono_audiosegments(left, right)
+    return audio_segment
+
+
+@app.route('/process', methods=['POST'])
+def process_file():
+    '''
+
+    process file from request after verifying proper file.
+    returns file with effects
+
+    '''
+
+
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+
+            filename = secure_filename(file.filename)
+
+            #audio segment from file and filetype
+            song=AudioSegment.from_file(file,file.filename.rsplit('.', 1)[1].lower())
+
+            #add effects to audio segment
+            processed_song=add_effects(song,int(request.form["type"]),int(request.form["intensity"]))
+
+
+
+
+
+            return send_file(processed_song,mimetype="audio/mp3")
+        else:
+
+            return redirect(request.url)
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ src/static/javascript/index.js
@@ -0,0 +1,220 @@
+(function(){
+
+let wavesurfers;
+function waveCreator(container,colors){
+  //wrapper for wavesurfer creator
+  waves = WaveSurfer.create({
+    container: container,
+    waveColor: colors[0],
+    progressColor: colors[1],
+    hideScrollbar:true
+ });
+ return waves
+}
+
+document.getElementById('submit').addEventListener('click',function(event){
+
+    //prevent from scrolling to top of page
+    event.preventDefault();
+
+    //alowed file types
+    const fileTypes=["mp3","wav","m4a","flac"]
+    
+    //get file and/or url from document
+    const file=document.getElementById("file");
+    const url=document.getElementById("url").value;
+
+    //create form for request
+    let form= new FormData();
+
+    let reqUrl;
+    if (file.files.length>0){
+
+       //verify file type
+       if( fileTypes.indexOf(file.files[0].name.slice(-3).toLowerCase())===-1){
+           alert("improper file type. currently supported file types are mp3, WAV, FLAC, m4a");
+           return;
+       }
+        //add file to form if file is present
+        form.append('file',file.files[0])
+        reqUrl='/process'
+
+
+    }
+    else if (url.length>5){
+        //get id from url
+        const id= url.split('=')[1]
+
+         //validate the proper length of id
+        if (id.length!==11){
+            alert("Invalid url")
+            return
+        }
+        // add id and url to form
+        form.append('id',id);
+        form.append('url',url);
+        reqUrl='/download'
+    }
+    else{
+        alert("no track uploaded or url selected");
+        return;
+    }
+
+    //possible dropdown menu values
+    const effectArray=["SLOWED + REVERB","BASS BOOST", "DISTORTED","SPED UP"]
+    const intensityArray=["NONE","LOW","MEDIUM","HIGH"]
+
+
+    let name=''
+    const type=document.getElementById("dropdownValue").innerText;
+    const intensity=document.getElementById("dropdownIValue").innerText;
+
+
+
+    //make sure drop down menus or populated
+    if (type==="SELECT EFFECT"||intensity==="SELECT LEVEL"){
+        alert("Effect and level must be selected");
+        return;
+    }
+
+    //view loader
+    $("#submit").hide();
+    $("#loader").show();
+
+    if (file.files.length>0){
+        name=`${file.files[0].name.slice(0,-4)} ${type}.mp3`;
+    }
+
+
+
+    //convert dropdown menu options to integers
+    form.append('type',effectArray.indexOf(type))
+    form.append('intensity',intensityArray.indexOf(intensity))
+
+    //request to server
+    axios({
+      method: 'post',
+      url: reqUrl,
+      data:form,
+      headers:{'Content-Type': 'multipart/form-data' },
+      responseType: 'blob',
+
+    })
+      .then((res) => {
+         if (reqUrl==='/download'){
+             //set file name
+             name= `${res.headers.name} ${type}.mp3`
+         }
+
+         //create waveform for returned song
+         blob = new Blob([res.data], { 'type' : 'audio/mp3' });
+         let wavesurfers=waveCreator('#waveforms',['violet','purple']);
+         wavesurfers.loadBlob(blob);
+
+
+
+         const fileDownloadButton=document.getElementById("download")
+         function download(){
+
+         // create download link and connect it to download button click
+
+            const anchor = document.createElement("a");
+            anchor.download = name;
+            anchor.href = window.URL.createObjectURL(blob);
+            anchor.target ="_blank";
+            anchor.style.display = "none"; // just to be safe!
+            document.body.appendChild(anchor);
+            anchor.click();
+            document.body.removeChild(anchor);
+         }
+
+
+
+         fileDownloadButton.addEventListener('click',download);
+         document.getElementById('play').addEventListener('click', function(input,event) {
+             //play and pause song on button click
+             wavesurfers.playPause();
+
+         });
+
+                //switch to track view from homepage
+                $("#loader").hide();
+                $("#submit").show();
+                $("#home").hide();
+                $("#songTitle").text(name);
+                $("#track").show();
+
+
+
+    })
+    .catch((err)=>{
+        alert(err);
+        $("#loader").hide();
+        $("#submit").show();
+        return;
+    }
+
+
+
+);
+
+
+
+
+
+
+
+});
+document.getElementById('file').addEventListener('change', function(input,event) {
+    // add file title to file button
+     $("#selectFile").text(input.target.files[0].name);
+
+
+
+
+});
+
+
+
+const $drowdownArrow = document.querySelector('.fa-angle-down');
+const $checkbox = document.getElementById('openDropdown');
+const $icheckbox = document.getElementById('i-openDropdown');
+const $idropdownMenu = document.querySelector('.i-dropdown-menu');
+const $dropdownMenu = document.querySelector('.dropdown-menu');
+
+
+
+$dropdownMenu.addEventListener('click', (e) => {
+  $checkbox.checked = false;
+  // setting checked to false won't trigger 'change'
+  // event, manually dispatch an event to rotate
+  // dropdown arrow icon
+  $checkbox.dispatchEvent(new Event('change'));
+});
+
+
+$idropdownMenu.addEventListener('click', (e) => {
+  $icheckbox.checked = false;
+  // setting checked to false won't trigger 'change'
+  // event, manually dispatch an event to rotate
+  // dropdown arrow icon
+  $icheckbox.dispatchEvent(new Event('change'));
+});
+const effectValues= document.querySelectorAll(".effectValue");
+effectValues.forEach((item, i) => {
+    item.addEventListener('click',function(){
+        //update dropdown menu text when an option is selected
+        document.getElementById("dropdownValue").innerHTML=item.innerText+'<i class="fa fa-arrow-down" style="color:#121212;font-size:25px"></i>'
+
+
+    });
+});
+const intensityValues= document.querySelectorAll(".intensityValue");
+intensityValues.forEach((item, i) => {
+    item.addEventListener('click',function(){
+        //update dropdown menu text when an option is selected
+        document.getElementById("dropdownIValue").innerHTML=item.innerText+'<i class="fa fa-arrow-down" style="color:#121212;font-size:25px;"></i>'
+
+});
+    });
+}());
